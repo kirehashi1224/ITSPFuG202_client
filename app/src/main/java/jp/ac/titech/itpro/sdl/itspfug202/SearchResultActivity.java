e6d2769a6c801073a4d50c8d3da4b1abfa0275d3
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-06-29 18:34:12 +0900

    帰ってきたwebAPIの値を検索結果画面にリストで表示

--- app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/SearchResultActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/SearchResultActivity.java
@@ -2,9 +2,13 @@ package jp.ac.titech.itpro.sdl.itspfug202;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import jp.ac.titech.itpro.sdl.itspfug202.model.Restaurant;
@@ -29,17 +33,26 @@ public class SearchResultActivity extends AppCompatActivity {
         Call<List<Restaurant>> restaurantsCall = apiService.getRestaurants();
         Log.d("Demo", "call apiService");
 
+        final RecyclerView recyclerView = findViewById(R.id.result_recycler);
+        LinearLayoutManager llManager = new LinearLayoutManager(this);
+        llManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llManager);
+
         restaurantsCall.enqueue(new Callback<List<Restaurant>>() {
             @Override
             public void onResponse(Call<List<Restaurant>> call, Response<List<Restaurant>> response) {
                 List<Restaurant> restaurants = response.body();
-                StringBuilder restaurantsDemo = new StringBuilder();
+                /*StringBuilder restaurantsDemo = new StringBuilder();
                 for (Restaurant r: restaurants) {
                     restaurantsDemo.append(r.toString());
+
                 }
                 Log.d("Demo", restaurantsDemo.toString());
                 TextView demo = findViewById(R.id.demo);
                 demo.setText(restaurantsDemo.toString());
+                */
+                RecyclerView.Adapter adapter = new SearchResultItemAdapter(restaurants);
+                recyclerView.setAdapter(adapter);
             }
 
             @Override
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/SearchResultItemAdapter.java
@@ -0,0 +1,46 @@
+package jp.ac.titech.itpro.sdl.itspfug202;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.ac.titech.itpro.sdl.itspfug202.model.Restaurant;
+
+public class SearchResultItemAdapter extends RecyclerView.Adapter<SearchResultItemViewHolder>{
+    private List<Restaurant> restaurantList;
+
+    public SearchResultItemAdapter(List<Restaurant> restaurantList){
+        this.restaurantList = restaurantList;
+    }
+
+    @NonNull
+    @Override
+    public SearchResultItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.search_result_item_layout, parent, false);
+        return new SearchResultItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchResultItemViewHolder holder, int position) {
+        holder.nameText.setText(restaurantList.get(position).getName());
+        holder.addressText.setText(restaurantList.get(position).getAddress());
+        holder.base.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "詳細は後で！！！！！！", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return restaurantList.size();
+    }
+}
