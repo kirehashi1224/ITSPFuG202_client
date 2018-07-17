cb42368e82b549799a824025eace20a8bb34a700
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-07-17 13:07:36 +0900

    MainActivityからランダムボタンでランダムに店を取得する機能を実装

--- app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/MainActivity.java
@@ -68,6 +68,16 @@ public class MainActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
+
+        Button randomButton = findViewById(R.id.randomButton);
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RestaurantDetailActivity.class);
+                intent.putExtra("random", "true");
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
