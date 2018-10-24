3cec3cf99cab67fabe81122a208c9dd1d223d304
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-10-24 15:12:29 +0900

    コードのインポートを最適化

--- app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/MainActivity.java
@@ -2,31 +2,13 @@ package jp.ac.titech.itpro.sdl.itspfug202;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import java.io.IOException;
-import java.util.List;
-
-import jp.ac.titech.itpro.sdl.itspfug202.model.Restaurant;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
