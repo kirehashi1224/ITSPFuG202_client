5e297e5872f0f6056c56d203a123b9c2e29d91ad
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-12-18 15:38:40 +0900

    APIを叩いてタグのリストを取得する処理を実装

--- app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/itspfug202/MainActivity.java
@@ -6,10 +6,23 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.List;
+
+import jp.ac.titech.itpro.sdl.itspfug202.model.DistanceTag;
+import jp.ac.titech.itpro.sdl.itspfug202.model.GenreTag;
+import jp.ac.titech.itpro.sdl.itspfug202.model.PriceTag;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +32,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BuildConfig.API_ADDRESS)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<List<PriceTag>> priceTagCall = apiService.getPriceTag();
+        Call<List<GenreTag>> genreTagCall = apiService.getGenreTag();
+        Call<List<DistanceTag>> distanceTagCall = apiService.getDistanceTag();
+
+        priceTagCall.enqueue(new Callback<List<PriceTag>>() {
+            @Override
+            public void onResponse(Call<List<PriceTag>> call, Response<List<PriceTag>> response) {
+                List<PriceTag> priceTagList = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<PriceTag>> call, Throwable t) {
+                sendNetworkErrorMessage();
+            }
+        });
+
+        genreTagCall.enqueue(new Callback<List<GenreTag>>() {
+            @Override
+            public void onResponse(Call<List<GenreTag>> call, Response<List<GenreTag>> response) {
+                List<GenreTag> genreTagList = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<GenreTag>> call, Throwable t) {
+                sendNetworkErrorMessage();
+            }
+        });
+
+        distanceTagCall.enqueue(new Callback<List<DistanceTag>>() {
+            @Override
+            public void onResponse(Call<List<DistanceTag>> call, Response<List<DistanceTag>> response) {
+                List<DistanceTag> distanceTagList = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<DistanceTag>> call, Throwable t) {
+                sendNetworkErrorMessage();
+            }
+        });
 
         Button searchButton = findViewById(R.id.searchButton);
         searchButton.setOnClickListener(new View.OnClickListener(){
@@ -52,5 +110,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private  void sendNetworkErrorMessage(){
+        Log.d("SearchResultActivity","onResponse_Failure");
+        Toast.makeText(getApplicationContext() , "ネットワークに接続されていません" , Toast.LENGTH_SHORT).show();
+    }
 
 }
